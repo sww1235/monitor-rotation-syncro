d4b8da7969b911d1c402be15ae966ec9484c1809
sww1235 <sww1235@gmail.com>
2017-05-25 19:27:57 -0600

    Added windows commands

--- Code/main.ino
+++ Code/main.ino
@@ -8,10 +8,17 @@
 const int input1 = 4;
 const int input2 = 5;
 
-// This allows the end user to specify what OS their computer is running.
-// Needed due to unique keyboard shortcuts between each OS
+// This allows the end user to specify what OS their computer is running. Needed
+// due to unique keyboard shortcuts between each OS.
+
 // 0 = windows, 1 = mac, 2 = linux
+
+// Only windows is implemented at present. macOS will need some additional
+// effort, as there is no built in keyboard shortcut to do this. Linux will vary
+// based on desktop environment.
 const int OS = 0;
+
+
 
 //logical 1 is VDD, usually 5V.
 
@@ -26,34 +33,109 @@ void setup() {
   Keyboard.begin();
 }
 
+int position = 0;
+// setting differently, so the code will run the first time.
+int lastPosition = 1;
+
 void loop() {
-  position = digitalRead(input1) + (2 * digitalRead(input2));
 
   // 0 + (2*0) = 0
   // 1 + (2*0) = 1
   // 0 + (2*1) = 2
   // 1 + (2*1) = 3
 
+  position = digitalRead(input1) + (2 * digitalRead(input2));
 
-// you may have to change which case does what based on the direction the sensor
-// is facing.
-  switch(position){
+  if (position != lastPosition){ // position has changed.
+
+  // you may have to change which case does what based on the direction the sensor
+  // is facing.
+    switch(position){
+      case 0:
+        rotateUp();
+        break;
+      case 1:
+        rotateLeft();
+        break;
+      case 2:
+        rotateDown();
+        break;
+      case 3:
+        rotateRight();
+        break;
+    }
+
+  }
+
+  lastPosition = position;
+}
+
+// key codes are at https://www.arduino.cc/en/Reference/KeyboardModifiers
+void rotateUp(){
+  switch(OS){
     case 0:
-
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press(KEY_LEFT_ALT);
+        Keyboard.press(Key_UP_ARROW);
+        delay(100);
+        Keyboard.releaseAll();
+        break;
       break;
     case 1:
-
       break;
     case 2:
-
       break;
-    case 3:
+  }
+}
 
+void rotateLeft(){
+  switch(OS){
+    case 0:
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press(Key_LEFT_ARROW);
+      delay(100);
+      Keyboard.releaseAll();
+      break;
+    case 1:
+      break;
+    case 2:
       break;
   }
 
 }
 
-void rotateLeft(){
+void rotateDown(){
+  switch(OS){
+    case 0:
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press(Key_DOWN_ARROW);
+      delay(100);
+      Keyboard.releaseAll();
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+  }
+
+}
+
+void rotateRight(){
+
+  switch(OS){
+    case 0:
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press(Key_RIGHT_ARROW);
+      delay(100);
+      Keyboard.releaseAll();
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+  }
 
 }
